e9ed1e8a4235fb5e8d374d6e8eb437b082bfdb35
wb-dv <isss.300@mail.ru>
2024-02-27 23:51:21 +0300

    add back link and configure his from global.js

--- src/components/UI/SocialLinks/SocialLinks.jsx
+++ src/components/UI/SocialLinks/SocialLinks.jsx
@@ -6,35 +6,42 @@ import { SocialLink } from '../SocialLink/SocialLink';
 import styles from './SocialLinks.module.scss';
 
 const socials = {
-  twitter: {
-    type: 'x',
-    href: 'https://twitter.com/myluckbitcoin',
-  },
-  site: {
-    type: 'site',
-    href: 'https://bitgesell.ca/',
-  },
-  telegram: {
-    type: 'telegram',
-    href: 'https://t.me/bitgesellofficial',
-  },
+    twitter: {
+        type: 'x',
+        href: 'https://twitter.com/myluckbitcoin',
+    },
+    site: {
+        type: 'site',
+        href: 'https://bitgesell.ca/',
+    },
+    telegram: {
+        type: 'telegram',
+        href: 'https://t.me/bitgesellofficial',
+    },
 };
 
+const backLink = window.backLink || 'https://bitgesell.fun/';
+
 export const SocialLinks = memo(function SocialLinks({ customClasses = '' }) {
-  return (
-    <div className={clsx(styles.SocialLinks, customClasses)}>
-      <SocialLink
-        type={socials.twitter.type}
-        href={socials.twitter.href}
-      />
-      <SocialLink
-        type={socials.site.type}
-        href={socials.site.href}
-      />
-      <SocialLink
-        type={socials.telegram.type}
-        href={socials.telegram.href}
-      />
-    </div>
-  );
+    return (
+        <div className={clsx(styles.SocialLinks__wrapper, customClasses)}>
+            <div className={styles.SocialLinks}>
+                <SocialLink
+                    type={socials.twitter.type}
+                    href={socials.twitter.href}
+                />
+                <SocialLink type={socials.site.type} href={socials.site.href} />
+                <SocialLink
+                    type={socials.telegram.type}
+                    href={socials.telegram.href}
+                />
+            </div>
+            <div className={styles.SocialLinks__backLink}>
+                And also
+                <a href={backLink} target='_blank' rel='noreferrer'>
+                    Bitgesell NFT Farm
+                </a>
+            </div>
+        </div>
+    );
 });
