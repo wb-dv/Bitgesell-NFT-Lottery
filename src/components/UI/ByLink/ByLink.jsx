b56c0d23072b75a00df4fd61f512839fe4ddf493
wb-dv <isss.300@mail.ru>
2024-02-27 22:48:51 +0300

    getg by link from global

--- src/components/UI/ByLink/ByLink.jsx
+++ src/components/UI/ByLink/ByLink.jsx
@@ -3,17 +3,20 @@ import clsx from 'clsx';
 
 import styles from './ByLink.module.scss';
 
+const link =
+    window.byLink || 'https://opensea.io/collection/new-bitgesell-road';
+
 export const ByLink = memo(function ByLink({ customClasses }) {
-  return (
-    <div className={styles.ByLink__wrapper}>
-      <a
-        className={clsx(styles.ByLink, customClasses)}
-        href="https://opensea.io/collection/new-bitgesell-road"
-        target="_blank"
-        rel="noreferrer"
-      >
-        get WBGL
-      </a>
-    </div>
-  );
+    return (
+        <div className={styles.ByLink__wrapper}>
+            <a
+                className={clsx(styles.ByLink, customClasses)}
+                href={link}
+                target='_blank'
+                rel='noreferrer'
+            >
+                get WBGL
+            </a>
+        </div>
+    );
 });
